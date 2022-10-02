003c06223f9abf7454b41cb83df0d3a035f0ef7a
Igor Tatarinov <62074491+TatarinovIgor@users.noreply.github.com>
2022-10-02 21:25:13 +0300

    header added

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import urllib.request
 from flask import Flask, render_template
 
-app = Flask(__name__, )
+app = Flask(__name__)
 
 
 @app.route('/')
 def output():
-    url = "http://api.open-notify.org/iss-now.json?callback="
+    url = "http://api.open-notify.org/iss-now.json"
     response = urllib.request.urlopen(url)
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 
