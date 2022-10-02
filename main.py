85ff1e757a5cd88ce36cc1d7f8e62f2c1eb2330e
IgorTatarinov <ig.tatarinov@gmail.com>
2022-10-02 18:10:39 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import urllib.request
 from flask import Flask, render_template
 
-app = Flask(__name__, )
+app = Flask(__name__)
 
 
 @app.route('/')
 def output():
-    url = "http://api.open-notify.org/iss-now.json"
+    url = "http://api.open-notify.org/iss-now.json?callback="
     response = urllib.request.urlopen(url)
     return response
 
